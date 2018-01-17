bce5878ae24f36c0b06498f2a0d9cffe7617238f
Tommy Cheng <cheyi02@ca.com>
2018-01-16 22:43:01 -0500

    read and update Domain

---
+++ readDomain.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. ./authn
+AUTHN="Authorization: Bearer ${TOKEN}"
+SMDOMAIN=$1
+curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN"
