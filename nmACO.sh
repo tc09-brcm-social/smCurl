09a4922219d1f9d328c2d43abb8a4eecc842a90d
Tommy Cheng <cheyi02@ca.com>
2018-02-01 11:43:51 -0500

    simplify nmACO.sh by using -S on ./jq

--- nmACO.sh
+++ nmACO.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #nmACO.sh normalize an existing ACO
 SMACO=$1
-SMACOBASE=`bash readACO.sh "$SMACO" | ./jq '.data | del(.Attributes)' | sed '$d'`
-SMACOATTR=`bash readACO.sh "$SMACO" | ./jq '.data.Attributes' |  sed '1d' |sed '$d' | sort | sed 's/"$/",/' | sed '$s/,$//'`
-echo "${SMACOBASE}" ", \"Attributes\" : [" "${SMACOATTR}" '] }' | ./jq '.'
+bash readACO.sh "$SMACO" | ./jq -S '.data'
