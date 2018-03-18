6a6e4ebee48f219e1321de38b14f4c37ce5e5e44
Tommy Cheng <Yihhwa.Cheng@ca.com>
2018-03-18 16:00:13 -0400

    use silent flag for SmUserDirectories read

--- SmUserDirectories/read.sh
+++ SmUserDirectories/read.sh
@@ -2,4 +2,4 @@
 . ./authn
 AUTHN="Authorization: Bearer ${TOKEN}"
 SMDIR=$1
-curl -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmUserDirectories/$SMDIR"
+curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmUserDirectories/$SMDIR"
