f26eaadcdb136ffe9cebdee3e45a75cae9875429
Tommy Cheng <Yihhwa.Cheng@ca.com>
2018-03-20 09:40:17 -0400

    SmAuthSchemes init

--- crAuthJson.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-JSON=$1
-. ./authn
-AUTHN="Authorization: Bearer ${TOKEN}"
-curl -k -X POST --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON  "https://$RESTHOST:${RESTPORT}/ca/api/sso/services/policy/v1/SmAuthSchemes"
