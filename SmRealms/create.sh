7fd1d1c28a8ca97701646a0948758b9dcb34bd24
Tommy Cheng <Yihhwa.Cheng@ca.com>
2018-03-20 12:12:50 -0400

    correct create.sh error

--- SmRealms/create.sh
+++ SmRealms/create.sh
@@ -3,4 +3,4 @@ SMDOMAIN=$1
 JSON=$2
 . ./authn
 AUTHN="Authorization: Bearer ${TOKEN}"
-curl -s -k -X POST --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON  "https://$RESTHOST:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN/SmRealms/""
+curl -s -k -X POST --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON  "https://$RESTHOST:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN/SmRealms/"
