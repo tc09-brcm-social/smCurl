23d1e8839ce9b9b13a73513ebc501e91d449c629
Tommy Cheng <Yihhwa.Cheng@ca.com>
2018-03-21 10:10:04 -0400

    SmPolicies create and template

--- SmPolicies/create.sh
+++ SmPolicies/create.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 . ./authn
 AUTHN="Authorization: Bearer ${TOKEN}"
-JSON=$$.json
 SMDOMAIN=$1
 SMREALM=$2
 SMRULE=$3
-SMDIR=$4
-bash policy.temp "$SMDOMAIN" "$SMREALM" "$SMRULE" "$SMDIR" > $JSON
+JSON=$4
 curl -k -X POST --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON  "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN/SmPolicies/"
