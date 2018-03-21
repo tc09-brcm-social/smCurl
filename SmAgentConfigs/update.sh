e49edf317b0d7e5f7186ecf5eb9d4771143ec077
Tommy Cheng <Yihhwa.Cheng@ca.com>
2018-03-21 10:11:29 -0400

    ACO update:

---
+++ SmAgentConfigs/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. ./authn
+AUTHN="Authorization: Bearer ${TOKEN}"
+SMACO=$1
+JSON=$2
+curl -s -k -X PUT --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmAgentConfigs/$SMACO"
--- updateACO.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. ./authn
-AUTHN="Authorization: Bearer ${TOKEN}"
-SMACO=$1
-JSON=$2
-curl -k -X PUT --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmAgentConfigs/$SMACO"
