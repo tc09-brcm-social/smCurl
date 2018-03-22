ecd3d817420bdf2f930d11863d82a4d62405db38
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-03-22 14:35:16 -0400

    SAML2 Entities init

---
+++ FedIdPLocals/create.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+JSON=$1
+. ./authn
+AUTHN="Authorization: Bearer ${TOKEN}"
+curl -s -k -X POST --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON  "https://$RESTHOST:${RESTPORT}/ca/api/sso/services/policy/v1/FedIdPLocals"
