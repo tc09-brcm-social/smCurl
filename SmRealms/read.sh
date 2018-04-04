5bbda74a6931c2300c8eca84380b2430162cb993
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-04-04 07:25:41 -0400

    modified SmRealm/read.sh to support list.sh

--- SmRealms/read.sh
+++ SmRealms/read.sh
@@ -3,4 +3,8 @@
 AUTHN="Authorization: Bearer ${TOKEN}"
 SMDOMAIN=$1
 Name=$2
-curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/${SMDOMAIN}/SmRealms/${Name}"
+if [[ -z "${SMDOMAIN}" ]]; then
+    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmRealms/"
+    else
+    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/${SMDOMAIN}/SmRealms/${Name}"
+fi
