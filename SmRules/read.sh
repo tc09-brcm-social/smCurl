78f2769bea468bbce122108482a8226a114e9d20
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-04-10 06:24:14 -0400

    SmRules supports list all rules of all domains

--- SmRules/read.sh
+++ SmRules/read.sh
@@ -4,4 +4,8 @@ AUTHN="Authorization: Bearer ${TOKEN}"
 SMDOMAIN=$1
 SMREALM=$2
 SMRULE=$3
-curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN/SmRealms/$SMREALM/SmRules/$SMRULE"
+if [[ -z "${SMDOMAIN}" ]]; then
+    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmRules/"
+    else
+    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN/SmRealms/$SMREALM/SmRules/$SMRULE"
+fi
