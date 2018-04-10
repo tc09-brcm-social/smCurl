bbf1515c6c9a16468429f1e2ef84af4afdedeaeb
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-04-10 06:28:48 -0400

    SmPolicies supports list.sh

--- SmPolicies/read.sh
+++ SmPolicies/read.sh
@@ -3,4 +3,8 @@
 AUTHN="Authorization: Bearer ${TOKEN}"
 SMDOMAIN=$1
 SMPOLICY=$2
-curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/${SMDOMAIN}/SmPolicies/${SMPOLICY}"
+if [[ -z "${SMDOMAIN}" ]]; then
+    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmPolicies/"
+    else
+    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/${SMDOMAIN}/SmPolicies/${SMPOLICY}"
+fi
