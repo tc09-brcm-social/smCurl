ecf704bb0e2a1b02ee7074e6471eb7695444408a
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-04-15 19:42:58 -0400

    added silence flag to the curl for SmPolicies update

--- SmPolicies/update.sh
+++ SmPolicies/update.sh
@@ -4,4 +4,4 @@ AUTHN="Authorization: Bearer ${TOKEN}"
 SMDOMAIN=$1
 SMPOLICY=$2
 JSON=$3
-curl -k -X PUT --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/${SMDOMAIN}/SmPolicies/${SMPOLICY}"
+curl -s -k -X PUT --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/${SMDOMAIN}/SmPolicies/${SMPOLICY}"
