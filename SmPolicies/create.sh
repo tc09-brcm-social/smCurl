5f9c3e5ea968410b631d835c012568ac1f1de7da
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-04-16 10:57:08 -0400

    added silent flag to Smpolicies create

--- SmPolicies/create.sh
+++ SmPolicies/create.sh
@@ -5,4 +5,4 @@ SMDOMAIN=$1
 SMREALM=$2
 SMRULE=$3
 JSON=$4
-curl -k -X POST --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON  "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN/SmPolicies/"
+curl -s -k -X POST --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON  "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN/SmPolicies/"
