dd7fde03ba2da464a3376de5e2e24c9760ae5378
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-05-02 00:48:53 -0400

    SmGlobal Policies and Realms init

---
+++ SmGlobalPolicies/read.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. ./authn
+AUTHN="Authorization: Bearer ${TOKEN}"
+SMDOMAIN=$1
+Name=$2
+if [[ -z "${SMDOMAIN}" ]]; then
+    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmGlobalPolicies/"
+    else
+    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/${SMDOMAIN}/SmGlobalPolicies/${Name}"
+fi
