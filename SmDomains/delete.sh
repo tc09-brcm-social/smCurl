d4a3a387adec259b2a806dc5ce4d8566aa30b10d
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-06-26 17:41:33 -0400

    adding silent flags to two delete.sh

--- SmDomains/delete.sh
+++ SmDomains/delete.sh
@@ -3,4 +3,4 @@ DIRNAME=$(cd "${BASH_SOURCE[0]%/*}"; pwd)
 . "${DIRNAME}/../authn"
 AUTHN="Authorization: Bearer ${TOKEN}"
 SMDOMAIN=$1
-curl -k -X DELETE --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN"
+curl -s -k -X DELETE --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN"
