a54d61e448bae9b43ce36f66ea9759a1c01083d8
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-09-27 18:26:09 +0000

    Azure AD Certificates Import from jks_uri

---
+++ examples/aad/README.md
@@ -0,0 +1,2 @@
+* bash makecert.sh # to extract/import certificates from AAD using kid as alias
+* sample.json is a sample json for  FedCertificates/create to work
---
+++ examples/aad/makecert.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mypwd=`pwd`
+cd ../..
+index=0
+for i in `wget -qO- https://login.microsoftonline.com/common/discovery/keys | ./jq '.keys  | .[] | [.x5c, .kid]' | grep '"'`
+do
+    index=$((index+1))
+    if [ $((index%2)) -eq 0 ]; then
+	JSON=$$.json
+	a1="${i%\"}"
+	alias="${a1#\"}"
+	cert1="${cert%\"}"
+	cert2="${cert1#\"}"
+	cert4=$"-----BEGIN CERTIFICATE-----\
+$cert2\
+-----END CERTIFICATE----"
+	i3=`echo -n "$cert4" |base64`
+	echo '{}' | ./jq --arg bar "$i3" --arg al $alias '. + {Alias: $al, Format: "PEM", CertificateData: $bar, "CertificateType": "Certificate", "type": "FedCertificate"}' > $JSON
+	bash FedCertificates/create.sh $JSON
+    else
+	cert=$i
+    fi
+done
