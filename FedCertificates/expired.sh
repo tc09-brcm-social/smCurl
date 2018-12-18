911c72cd91d8bcab6384e779c53f08c1c89cdc99
Tommy Cheng <YihHwa.Cheng@ca.com>
2018-12-18 12:45:00 -0500

    expired certificate under FedCertificates

---
+++ FedCertificates/expired.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+MYPATH=`dirname $0`
+for i in `bash ${MYPATH}/list.sh | grep Fed | awk 'BEGIN {FS="\""; } {print $2;}' | awk 'BEGIN {FS="/";} {print $3;}'`
+do
+    MY=`bash ${MYPATH}/read.sh $i | ./jq '.data.ValidTill'`
+    MY1="${MY%\"}"
+    MYD="${MY1#\"}"
+    if [[ `date --date="$MYD" +"%s"` < `date +"%s"` ]]
+    then
+       echo -n "$i $MY"
+    fi
+done
