2782e464d935ac27453899b6edf43cd5045cc74b
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-06-23 17:18:03 +0000

    Fed Partnership creates

---
+++ FedSPPartnerships/create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DIRNAME=$(cd "${BASH_SOURCE[0]%/*}"; pwd)
+. "${DIRNAME}/../authn"
+AUTHN="Authorization: Bearer ${TOKEN}"
+JSON=$1
+curl -s -k -X POST --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/FedSPPartnerships"
