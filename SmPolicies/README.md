b09b2cc81408e1f5210845db128e2108535bbbbf
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-06-28 21:28:54 +0000

    SmPolicies: maketemp.sh

---
+++ SmPolicies/README.md
@@ -0,0 +1,3 @@
+* maketemp
+** does not do complete job, however, the output could does capture
+** the basic framework of a template.
---
+++ SmPolicies/maketemp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+MYPATH=`dirname $0`
+SMDOMAIN=$1
+SMPOL=$2
+JSON=$$.json
+bash "${MYPATH}"/read.sh "$SMDOMAIN" "$SMPOL" | ./jq '.data' > "$JSON"
+UP=` ./jq '.SmUserPolicies[]|[. + {UserDirectory: { path: "/SmUserDirectories/$SMDIR"}}]' "$JSON"`
+PL='[{ "type": "SmPolicyLink",
+        "RuleLink": {
+          "path": "/SmDomains/$SMDOMAIN/SmRealms/$SMREALM/SmRules/$SMRULE"
+        }
+      }]'
+echo '#!/bin/bash'
+echo 'Name=$1'
+echo 'SMDOMAIN=$2'
+echo 'SMREALM=$3'
+echo 'SMRULE=$4'
+echo 'SMDIR=$5'
+echo 'cat <<EOF'
+bash "${MYPATH}/read.sh" "$SMDOMAIN" "$SMPOL" | \
+	./jq --argjson up "$UP" --argjson pl "$PL"\
+	      '.data + {Name: "$Name",
+	      SmPolicyLinks: $pl,
+	      SmUserPolicies: $up }' | ./jq -f jqlib/rmIHD.jq
+echo EOF
