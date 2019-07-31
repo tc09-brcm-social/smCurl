71d939a5a9942794360f7cac20f7f89dd0e74045
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-07-31 22:07:13 +0000

    SmVariables: new relevations about list.sh that distinguish object and links

---
+++ SmVariables/exist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+MYPATH=`dirname $0`
+NAME=$1
+CHILD=$2
+READ=`bash "${MYPATH}/read.sh" "$NAME" "$CHILD"`
+echo "$READ"
+RESP=`echo "$READ" | ./jq -r '.responseType'`
+if [ "$RESP" == "error" ]; then
+    >&2 echo "$CHILD of $NAME does not exist."
+    exit 1
+fi
--- SmVariables/list.sh
+++ SmVariables/list.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 MYPATH=`dirname $0`
-SMDOMAIN=$1
-bash "${MYPATH}/read.sh" "$SMDOMAIN" | ./jq '.data | [.[]| .path]'
+NAME=$1
+CHILD=$2
+EXIST=`bash "${MYPATH}/exist.sh" "$NAME" "$CHILD"`
+if [[ $? != 0 ]]; then
+    ./jq -n '. + []'
+    exit 1
+fi
+RESP=`echo "$EXIST" | ./jq -r '.responseType'`
+if [ "$RESP" == "object" ]; then
+    echo "$EXIST" | ./jq '[ .parent.path + "/" + .data.type + "s/" + .data.Name]'
+fi
+if [ "$RESP" == "links" ]; then
+    echo "$EXIST" | ./jq '.data | [.[]| .path]'
+fi
