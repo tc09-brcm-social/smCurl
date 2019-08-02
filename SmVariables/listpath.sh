8513a1fcef2dc6673da16d143a63e9b29b6a09f2
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-08-02 21:40:34 +0000

    SmVariables: listpath.sh and listobj.sh now uses list.sh

--- SmVariables/listpath.sh
+++ SmVariables/listpath.sh
@@ -2,16 +2,9 @@
 MYPATH=`dirname $0`
 NAME=$1
 CHILD=$2
-EXIST=`bash "${MYPATH}/exist.sh" "$NAME" "$CHILD"`
+LIST=`bash "${MYPATH}/list.sh" "$NAME" "$CHILD"`
 if [[ $? != 0 ]]; then
     ./jq -n '. + []'
     exit 1
 fi
-RESP=`echo "$EXIST" | ./jq -r '.responseType'`
-if [ "$RESP" == "object" ]; then
-    OBJPATH=`echo "$EXIST" | ./jq '.parent.path + "/" + .data.type + "s/" + .data.Name'`
-    echo "$OBJPATH" | ./jq " [ { path: . } ]"
-fi
-if [ "$RESP" == "links" ]; then
-    echo "$EXIST" | ./jq '.data | [.[]| { path: .path} ]'
-fi
+echo "$LIST" | ./jq '[.[]| { path: .} ]'
