0148070fb799e11a5b935eb32e6587dbb344125a
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-08-04 14:47:05 +0000

    SmPolicies: create.sh only takes Domain and JSON file

--- SmPolicies/create.sh
+++ SmPolicies/create.sh
@@ -3,7 +3,5 @@ DIRNAME=$(cd "${BASH_SOURCE[0]%/*}"; pwd)
 . "${DIRNAME}/../authn"
 AUTHN="Authorization: Bearer ${TOKEN}"
 SMDOMAIN=$1
-SMREALM=$2
-SMRULE=$3
-JSON=$4
+JSON=$2
 curl -s -k -X POST --header 'Accept: application/json' --header "${AUTHN}" -d @$JSON  "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/$SMDOMAIN/SmPolicies/"
