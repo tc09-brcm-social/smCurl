a9d2c82384f40bd434be311ae8f2ba4c4e4c64ca
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-08-05 21:35:06 +0000

    SmAgentConfigs: start removing legacy backticks practices

--- SmAgentConfigs/exist.sh
+++ SmAgentConfigs/exist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-MYPATH=`dirname $0`
+MYPATH=$(dirname "$0")
 NAME=$1
-READ=`bash "${MYPATH}/read.sh" "$NAME"`
+READ=$(bash "${MYPATH}/read.sh" "$NAME")
 echo "$READ"
-RESP=`echo "$READ" | ./jq '.responseType'`
-if [ "$RESP" == "\"error\"" ]; then
+RESP=$(echo "$READ" | ./jq -r '.responseType')
+if [ "$RESP" == "error" ]; then
     >&2 echo "$NAME does not exist."
     exit 1
 fi
