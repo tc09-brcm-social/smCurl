f838a76c350eec7abb1fd23c8dc7a64a601db9f9
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-08-18 16:56:37 +0000

    FedCertificates: export.sh

--- FedCertificates/export.sh
+++ FedCertificates/export.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-Name=$1
-Fmt=$2
-Pwd=$3
+JSON=$1
 DIRNAME=$(cd "${BASH_SOURCE[0]%/*}"; pwd)
 . "${DIRNAME}/../authn"
 AUTHN="Authorization: Bearer ${TOKEN}"
-JSON=$$.json
-./jq --arg a "$Name" --arg f "$Fmt" --arg p "$Pwd" \
-        -n '. + { Alias: $a, Password: $p, Format: $f }' > "$JSON"
-curl -s -k -X POST --header "${AUTHN}" -d @$JSON  "https://$RESTHOST:${RESTPORT}/ca/api/sso/services/policy/v1/FedCertificates"
-
+curl -s -k -X POST --header "${AUTHN}" \
+    -d @"$JSON" \
+    "https://$RESTHOST:${RESTPORT}/ca/api/sso/services/policy/v1/FedCertificates"
