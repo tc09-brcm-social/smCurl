e94cf2e952a9b21aaf2f6a4e00cd29d90f555ec4
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-08-30 00:24:28 +0000

    SmPolicies: read.sh reformatting script

--- SmPolicies/read.sh
+++ SmPolicies/read.sh
@@ -6,7 +6,11 @@ AUTHN="Authorization: Bearer ${TOKEN}"
 NAME=$1
 CHILD=$2
 if [[ -z "${NAME}" ]]; then
-    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/${MYBASE}/"
+    curl -s -k -X GET --header 'Accept: application/json' \
+        --header "${AUTHN}" \
+        "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/${MYBASE}/"
     else
-    curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/${NAME}/${MYBASE}/${CHILD}"
+    curl -s -k -X GET --header 'Accept: application/json' \
+        --header "${AUTHN}" \
+        "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmDomains/${NAME}/${MYBASE}/${CHILD}"
 fi
