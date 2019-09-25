a0ff71d79ba79cbe87162051d3353a4551e1edac
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-09-25 16:16:54 +0000

    SmUserDirectories: catching up SmAgents template

--- SmUserDirectories/create.sh
+++ SmUserDirectories/create.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 JSON=$1
 DIRNAME=$(cd "${BASH_SOURCE[0]%/*}"; pwd)
+MYBASE=$(basename "$DIRNAME")
 . "${DIRNAME}/../authn"
 AUTHN="Authorization: Bearer ${TOKEN}"
 curl -s -k -X POST --header 'Accept: application/json' \
     --header 'Content-Type: application/json' \
     --header "${AUTHN}" -d @"$JSON" \
-    "https://$RESTHOST:${RESTPORT}/ca/api/sso/services/policy/v1/SmUserDirectories"
+    "https://$RESTHOST:${RESTPORT}/ca/api/sso/services/policy/v1/${MYBASE}"
--- SmUserDirectories/update.sh
+++ SmUserDirectories/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIRNAME=$(cd "${BASH_SOURCE[0]%/*}"; pwd)
+MYBASE=$(basename "$DIRNAME")
 . "${DIRNAME}/../authn"
 AUTHN="Authorization: Bearer ${TOKEN}"
 NAME=$1
@@ -7,4 +8,4 @@ JSON=$2
 curl -s -k -X PUT --header 'Accept: application/json' \
     --header 'Content-Type: application/json' \
     --header "${AUTHN}" -d @"$JSON" \
-    "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmUserDirectories/$NAME"
+    "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/$MYBASE/$NAME"
