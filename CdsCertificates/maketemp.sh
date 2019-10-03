53be611610a268fdaba3ded476620f5aee9bbf08
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-10-03 16:55:11 +0000

    CdsCertificates: removed for now

--- CdsCertificates/maketemp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-Alias=$1
-echo '#!/bin/bash'
-echo 'Alias=$1'
-echo 'cat <<EOF'
-MYPATH=`dirname $0`
-bash "${MYPATH}/read.sh" "$Alias" | ./jq -S '.data | del(.id) | .Alias = "${Alias}"'
-echo 'EOF'
