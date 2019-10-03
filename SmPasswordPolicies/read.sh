78fccd37a412d879379ae0f20415b90d9b113737
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-10-03 16:46:01 +0000

    SmPasswordPolicies: removed for now

--- SmPasswordPolicies/read.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIRNAME=$(cd "${BASH_SOURCE[0]%/*}"; pwd)
-. "${DIRNAME}/../authn"
-AUTHN="Authorization: Bearer ${TOKEN}"
-Name=$1
-curl -s -k -X GET --header 'Accept: application/json' --header "${AUTHN}" "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmPasswordPolicies/$Name"
