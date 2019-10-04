0dd2f1d75a9b5457557745e4918c1873ef0af096
Tommy Cheng <yihhwa.cheng@broadcom.com>
2019-10-04 22:26:34 +0000

    cleanup isOK.sh

--- isOK.sh
+++ isOK.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-DIRNAME=$(cd "${BASH_SOURCE[0]%/*}"; pwd)
-. "${DIRNAME}/authn"
+MYPATH=$(dirname "$0")
+. "${MYPATH}/authn"
 AUTHN="Authorization: Bearer ${TOKEN}"
-curl -k -X GET --header 'Accept: application/json' --header "${AUTHN}" \
-"https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmAdmins"
-echo This is to verify whether authn is set correctly.
-echo If the result of the run is ok, ${RESTHOST} and ${TOKEN} work together
+RESP=$(curl -v -k -X GET --header 'Accept: application/json' --header "${AUTHN}" \
+"https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmAdmins")
+>&2 echo This is to verify whether file \"authn\" is set correctly.
+>&2 echo If it works, it is using an Access Token:
+>&2 echo "${TOKEN}"
+>&2 echo to connect to \"${RESTHOST} : ${RESTPORT}\"
+>&2 echo A list of SmAdmins retrieved:
+echo "$RESP" |./jq '.data | .[] | .path'
