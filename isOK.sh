63b82540b5a899ac783d99822721234e6dea9bf1
Tommy Cheng <yihhwa.cheng@broadcom.com>
2020-04-16 09:48:47 -0700

    isOK.sh : change the order of messages

--- isOK.sh
+++ isOK.sh
@@ -4,9 +4,9 @@ MYPATH=$(dirname "$0")
 AUTHN="Authorization: Bearer ${TOKEN}"
 RESP=$(curl ${OPT} --header "host: ${RESTHOST}" -v -k -X GET --header 'Accept: application/json' --header "${AUTHN}" \
 "https://${RESTHOST}:${RESTPORT}/ca/api/sso/services/policy/v1/SmAdmins")
+echo "$RESP" |./jq '.data | .[] | .path'
+>&2 echo A list of SmAdmins retrieved above.
 >&2 echo This is to verify whether file \"authn\" is set correctly.
 >&2 echo If it works, it is using an Access Token:
 >&2 echo "${TOKEN}"
 >&2 echo to connect to \"${RESTHOST} : ${RESTPORT}\"
->&2 echo A list of SmAdmins retrieved:
-echo "$RESP" |./jq '.data | .[] | .path'
