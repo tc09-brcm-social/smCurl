135b2b4fcee87cb60ed20340d693a0349241b4e6
Tommy Cheng <yihhwa.cheng@broadcom.com>
2020-04-17 10:47:34 -0700

    examples/apache: uses examples/wa

---
+++ examples/apache/README.md
@@ -0,0 +1,7 @@
+# examples/apache
+* acoApache.temp -- a template in case a live WAACODEF does not exist
+* b4config.sh --  use the examples/wa to create the three objects needed
+	* before running the Apache Web Agent Config Wizard
+* env.shlib
+	* this env.shlib echos the variables setting to be appended to
+	* wa/env.shlib to change the setting of the examples/wa.
--- examples/apache/b4config.sh
+++ examples/apache/b4config.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
-. ./env.shlib
 MYPWD=$(pwd)
+cd ../wa
+. "$MYPWD/env.shlib" >> env.shlib
+WAPWD=$(pwd)
+bash ./make.sh
+cd ${WAPWD}
+. ./env.shlib
 cd ../..
-#
-# HCO for WA
-#
-SMHCO=$WAHCO
-if ! EXIST=$(bash SmHostConfigs/exist.sh "$SMHCO"); then
-    SMPSHOST=$WAPSHOST
-    JSON=$$.json
-    bash SmHostConfigs/temp/hco.temp "$SMHCO" "$SMPSHOST" >"$JSON"
-    EXIST=$(bash SmHostConfigs/create.sh "$JSON")
-fi
-echo "$EXIST" | ./jq '.data'
-#
-# Agent: WAAGENT
-#
-SMAGENT=$WAAGENT
-if ! EXIST=$(bash SmAgents/exist.sh "$SMAGENT"); then
-    SMAGENTTEMP=SmAgents/temp/agent.temp
-    JSON=$$.json
-    bash "$SMAGENTTEMP" "$SMAGENT" >"$JSON"
-    EXIST=$(bash SmAgents/create.sh "$JSON")
-fi
-echo "$EXIST" | ./jq '.data'
-#
-# ACO - WAACO
-#
 SMACO=$WAACO
-if ! EXIST=$(bash SmAgentConfigs/exist.sh "$SMACO"); then
-    SMACOTEMP="$MYPWD/acoApache.temp"
+if EXIST=$(bash SmAgentConfigs/exist.sh "$SMACO"); then
     JSON=$$.json
-    bash "$SMACOTEMP" "$SMACO" "$SMAGENT" "$WADEF" > "$JSON"
-    EXIST=$(bash SmAgentConfigs/create.sh "$JSON")
+    echo "$EXIST" | ./jq '.data' | bash SmAgentConfigs/cleanse.sh | \
+        bash SmAgentConfigs/jsetattr.sh LogoffUri '%2Flogout.html' | \
+        bash SmAgentConfigs/jaddattr.sh LogoffUri '%2Flogoff%2Flogoff.html' \
+        > "$JSON"
+    EXIST=$(bash SmAgentConfigs/update.sh "$SMACO" "$JSON")
 fi
 echo "$EXIST" | ./jq '.data'
