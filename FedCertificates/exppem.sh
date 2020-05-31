85d63ab600c50db7d976d07fa3cbfa90771d387c
Tommy Cheng <yihhwa.cheng@broadcom.com>
2020-05-31 11:41:39 +0000

    FedCertificates: remove duplicated error message

--- FedCertificates/exppem.sh
+++ FedCertificates/exppem.sh
@@ -4,7 +4,7 @@ NAME=$1
 EXIST=$(bash "${MYPATH}/exist.sh" "$NAME")
 RESP=$(echo "$EXIST" | ./jq -r '.responseType')
 if [ "$RESP" == "error" ]; then
-    >&2 echo "$NAME does not exist."
+#    >&2 echo "$NAME does not exist."
     exit 1
 fi
 JSON=$$.json
