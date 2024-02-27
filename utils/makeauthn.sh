b39186aadc431522114defbc80168303f18cfa8a
Tommy Cheng <yihhwa_cheng@broadcom.com>
2024-02-27 11:32:37 +0000

    utils: makeauthn, fix a typo

--- utils/makeauthn.sh
+++ utils/makeauthn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . utils/env.shlib
->&2 echo "Setting up $(PWD)/authn to connect to $HOST $PORT"
+>&2 echo "Setting up $(pwd)/authn to connect to $HOST $PORT"
 bash utils/authn.sh "$HOST" "$PORT" "$ID" "$PASS" > authn
