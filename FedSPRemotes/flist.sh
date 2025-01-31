f5239615b8c827f18d25c388908eaac283e88b01
Tommy Cheng <yihhwa.cheng@broadcom.com>
2025-01-31 01:53:24 +0000

    FedSPRemotes: filter support fexist, fread, flist

---
+++ FedSPRemotes/flist.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+NAME=$1
+MYPATH=$(dirname "$0")
+if ! EXIST=$(bash "${MYPATH}/fexist.sh" "$NAME"); then
+    STATUS=$?
+    ./jq -n '. + []'
+    exit "$STATUS"
+fi
+RESP=$(echo "$EXIST" | ./jq -r '.responseType')
+if [ "$RESP" == "object" ]; then
+    echo "$EXIST" | ./jq '[ .parent.path + "/" + .data.type + "s/" + .data.Name]'
+fi
+if [ "$RESP" == "links" ]; then
+    DATA=$(echo "$EXIST" | ./jq '.data')
+    if [ "$DATA" == "null" ]; then
+        >&2 echo "$NAME does not exist."
+        ./jq -n '. + []'
+        exit 1
+    fi
+    echo "$DATA" | ./jq '[.[]| .path]'
+fi
